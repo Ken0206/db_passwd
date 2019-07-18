0774bc3ebd0f22a6aaa2a9b22fdc483f558ff5ea
Ken Peng <cryptdoc@gmail.com>
2019-07-18 16:37:24 +0800

    改使用 sed ，更簡潔

--- db_passwd.sh
+++ db_passwd.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 # watch password file , if password file changed then modify config file
-# date : 2019-03-11
-
-password_change() {
-  conf_temp="${0%/*}/${RANDOM}.conf.temp"
-  new_passwd=$(cat ${password_file})
-  grep -v "7,DB2 DB USER PASSWORD" ${conf_file} > ${conf_temp}
-  echo "7,DB2 DB USER PASSWORD  :10-${new_passwd}" >> ${conf_temp}
-  cat ${conf_temp} > ${conf_file}
-  rm -f ${conf_temp}
-}
-
+# date : 2019-07-18
 
 conf_file="${0%/*}/conf_file"
 password_file="${0%/*}/password.txt"
@@ -20,7 +10,7 @@ while [ True ] ; do
   sleep 1
   new_password=$(cat ${password_file})
   if [ "${original_password}" != "${new_password}" ] ; then
-    password_change
+    sed -i "s/7,DB2 DB USER PASSWORD  :10-........../7,DB2 DB USER PASSWORD  :10-${new_password}/g" ${conf_file}
     original_password="${new_password}"
   fi
 done
